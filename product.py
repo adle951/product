d77c455cef8a6facaa351fd31ece39902fc33b28
jing chen <adle9511@gmail.com>
2022-08-11 16:10:16 +0800

    '完整句式顯示'

--- product.py
+++ product.py
@@ -5,6 +5,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入價格')
+	#清單簡寫 p = [name, price] or product.append([name, price])
 	p = []
 	p.append(name)
 	p.append(price)
@@ -12,3 +13,6 @@ while True:
 print(product)
 print(product[0][0])
 
+for p in product:
+	print(p[0], '價格是:', p[1])
+print(p)
