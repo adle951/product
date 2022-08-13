87093923b519f0e1df0cc76499ca3e377e84f808
jing chen <adle9511@gmail.com>
2022-08-13 14:32:18 +0800

    增加欄位說明

--- product.py
+++ product.py
@@ -16,3 +16,10 @@ print(product[0][0])
 for p in product:
 	print(p[0], '價格是:', p[1])
 print(p)
+
+
+#存入
+with open('product.csv', 'w', encoding ='utf-8') as f:
+	f.write('商品,價格\n')
+	for p in product:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
